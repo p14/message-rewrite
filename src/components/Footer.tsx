3f042ac8572551b847be97c381933200453209c2
Joseph Perez <joseph@p14.dev>
2023-01-02 22:18:48 -0800

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,9 @@ import { descriptionStyles } from '../styles/generic.styles';
 const Footer: React.FC = () => {
   return (
     <Box sx={descriptionStyles}>
+      <Typography variant='caption'>
+        Your message is sent to <a href='https://openai.com' target='_blank' rel='noreferrer'>OpenAI</a> who charges about $0.002 to process each request. Thank you for your support.
+      </Typography>
       <Typography variant='body1'>
         Created by Joseph Perez - <a href='https://www.github.com/p14/message-rewrite' target='_blank' rel='noreferrer'>Source Code</a>
       </Typography>
